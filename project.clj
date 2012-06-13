7229d4d378e025e3e98f3770d54a78476e6a6677
Kushal Pisavadia <kushi.p@gmail.com>
2012-06-13 23:18:03 +0100

    Add dependencies to be used
    These are the dependencies which are likely to be used for the various
    bits of the web app.

--- project.clj
+++ project.clj
@@ -1,3 +1,9 @@
 (defproject addressbook "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "A Restful web app which holds and retrieves contact information"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.2"]
+                 [congomongo "0.1.9"]
+                 [de.ubercode.clostache/clostache "1.3.0"]
+                 [compojure "1.1.0"]]
+  :dev-dependencies [[lein-ring "0.7.1"]
+                     [ring-mock "0.1.2"]])
