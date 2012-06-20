3fa4d8c7be8be78100b66c2be5c0a2164e95b98d
Kushal Pisavadia <kushi.p@gmail.com>
2012-06-20 10:10:37 +0100

    Use test-record fixture

--- test/addressbook/test/core.clj
+++ test/addressbook/test/core.clj
@@ -1,35 +1,14 @@
 (ns addressbook.test.core
   (:use [addressbook.core]
         [addressbook.data]
+        [addressbook.test.fixtures]
         [clojure.test]
         [ring.mock.request]
         [somnium.congomongo])
   (:require [clojure.data.json :as json]))
 
 (testing "Restful endpoints for handlers"
-  (let [test-record {:name {:family "Gump"
-                            :given "Forrest"
-                            :additional nil
-                            :prefix nil
-                            :suffix nil}
-                     :formatted-name "Forrest Gump"
-                     :org "Bubba Gump Shrimp Co."
-                     :title "Shrimp Man"
-                     :photo "http://www.example.com/dir_photos/my_photo.gif"
-                     :tel [{:type "work,voice"
-                            :value "+1-111-555-1212"}
-                           {:type "home,voice"
-                            :value "+1-404-555-1212"}]
-                     :address [{:type "work"
-                                :label "42 Plantation St.\nBaytown, LA 30314\nUnited States of America"
-                                :street "42 Plantation St."
-                                :locality "Baytown"
-                                :region "LA"
-                                :code "30314"
-                                :country "United States of America"}]
-                     :email "forrestgump@example.com"
-                     :rev "20080424T195243Z"}
-        test-conn (make-connection "contacts-development"
+  (let [test-conn (make-connection "contacts-development"
                                    :host "127.0.0.1"
                                    :port 27017)
         init-db (do (set-write-concern test-conn :safe)
@@ -46,7 +25,7 @@
 
     (deftest contact-with-id-produces-json-response
       (is (= 200 (:status (app (request :get (str "/contact/" id))))))
-      (is (= "{\"name\":{\"prefix\":null,\"family\":\"Gump\",\"suffix\":null,\"additional\":null,\"given\":\"Forrest\"},\"org\":\"Bubba Gump Shrimp Co.\",\"photo\":\"http:\\/\\/www.example.com\\/dir_photos\\/my_photo.gif\",\"title\":\"Shrimp Man\",\"email\":\"forrestgump@example.com\",\"rev\":\"20080424T195243Z\",\"address\":[{\"country\":\"United States of America\",\"locality\":\"Baytown\",\"code\":\"30314\",\"street\":\"42 Plantation St.\",\"type\":\"work\",\"region\":\"LA\",\"label\":\"42 Plantation St.\\nBaytown, LA 30314\\nUnited States of America\"}],\"formatted-name\":\"Forrest Gump\",\"tel\":[{\"type\":\"work,voice\",\"value\":\"+1-111-555-1212\"},{\"type\":\"home,voice\",\"value\":\"+1-404-555-1212\"}]}"
+      (is (= "{\"name\":{\"prefix\":\"\",\"family\":\"Gump\",\"suffix\":\"\",\"additional\":\"\",\"given\":\"Forrest\"},\"org\":\"Bubba Gump Shrimp Co.\",\"photo\":\"http:\\/\\/www.example.com\\/dir_photos\\/my_photo.gif\",\"title\":\"Shrimp Man\",\"email\":\"forrestgump@example.com\",\"rev\":\"20080424T195243Z\",\"address\":[{\"country\":\"United States of America\",\"locality\":\"Baytown\",\"code\":\"30314\",\"street\":\"42 Plantation St.\",\"type\":\"work\",\"region\":\"LA\",\"label\":\"42 Plantation St.\\nBaytown, LA 30314\\nUnited States of America\"}],\"formatted-name\":\"Forrest Gump\",\"tel\":[{\"type\":\"work,voice\",\"value\":\"+1-111-555-1212\"},{\"type\":\"home,voice\",\"value\":\"+1-404-555-1212\"}]}"
              ;; remove the :_id as the response may have been created
              ;; asas part of the test.
              (-> (:body (app (request :get (str "/contact/" id))))
