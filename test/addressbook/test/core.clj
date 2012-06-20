77ef8878a562f03214ce81fda820cd53a5be2b8b
Kushal Pisavadia <kushi.p@gmail.com>
2012-06-20 21:18:30 +0100

    Wrap tests in (testing ...) blocks
    Make it easier to spot which tests are related by pointing to each
    block of tests. Where possible it should be easy to map tests to
    their relevant endpoints.

--- test/addressbook/test/core.clj
+++ test/addressbook/test/core.clj
@@ -19,42 +19,45 @@
              (if (not (nil? record))
                (:_id record)
                (:_id (add-contact test-record))))]
-    (deftest base-route-returns-docs
-      (is (= 200 (:status (app (request :get "/")))))
-      (is (= "<h1>TODO: Write docs</h1>" (:body (app (request :get "/"))))))
+    (testing "GET / (homepage)"
+      (deftest base-route-returns-docs
+        (is (= 200 (:status (app (request :get "/")))))
+        (is (= "<h1>TODO: Write docs</h1>" (:body (app (request :get "/")))))))
 
-    (deftest contact-with-id-produces-json-response
-      (is (= 200 (:status (app (request :get (str "/contact/" id))))))
-      (is (= "{\"name\":{\"prefix\":\"\",\"family\":\"Gump\",\"suffix\":\"\",\"additional\":\"\",\"given\":\"Forrest\"},\"org\":\"Bubba Gump Shrimp Co.\",\"photo\":\"http:\\/\\/www.example.com\\/dir_photos\\/my_photo.gif\",\"title\":\"Shrimp Man\",\"email\":\"forrestgump@example.com\",\"rev\":\"20080424T195243Z\",\"address\":[{\"country\":\"United States of America\",\"locality\":\"Baytown\",\"code\":\"30314\",\"street\":\"42 Plantation St.\",\"type\":\"work\",\"region\":\"LA\",\"label\":\"42 Plantation St.\\nBaytown, LA 30314\\nUnited States of America\"}],\"formatted-name\":\"Forrest Gump\",\"tel\":[{\"type\":\"work,voice\",\"value\":\"+1-111-555-1212\"},{\"type\":\"home,voice\",\"value\":\"+1-404-555-1212\"}]}"
-             ;; remove the :_id as the response may have been created
-             ;; asas part of the test.
-             (-> (:body (app (request :get (str "/contact/" id))))
-                 (json/read-json)
-                 (dissoc :_id)
-                 (json/json-str)))))
-
-    (deftest contact-with-bad-id-produces-error-response
-      (is (= 404 (:status (app (request :get "/contact/meh")))))
-      (is (= "{\"error\":{\"message\":\"contact with id: 'meh' not found\"}}"
-             (:body (app (request :get "/contact/meh"))))))
-
-    (deftest contact-with-id-and-vcard-produces-vcard-response
-      (is (= 200 (:status (app (request :get (str "/contact/" id "/vcard"))))))
-      (is (= "BEGIN:VCARD\nVERSION:4.0\nN:Gump;Forrest;;;\nFN: Forrest Gump\nORG:Bubba Gump Shrimp Co.\nTITLE:Shrimp Man\nPHOTO:http://www.example.com/dir_photos/my_photo.gif\nTEL;TYPE=\"work,voice\";VALUE=uri:tel:+1-111-555-1212\nTEL;TYPE=\"home,voice\";VALUE=uri:tel:+1-404-555-1212\nADR;TYPE=work;LABEL=\"42 Plantation St.\nBaytown, LA 30314\nUnited States of America\"\n :;;42 Plantation St.;Baytown;LA;30314;United States of America\nEMAIL:forrestgump@example.com\nREV:20080424T195243Z\nEND:VCARD"
-             (:body (app (request :get (str "/contact/" id "/vcard")))))))
-
-    (deftest contact-is-created-on-post
-      (let [response (app (-> (request :post "/contact")
-                              (header "content-type" "application/x-www-form-urlencoded")
-                              (body (update-in test-record [:formatted-name] (fn [_] "Dummy")))))]
-        (is (= {"Content-Type" "application/json"} (:headers response)))
-        ;; Drop the :id key as this is likely to be dynamically generated
-        ;; for each test run.
-        (is (= {:message "contact created"}
-               (-> (:body response)
+    (testing "GET /contact/:id"
+      (deftest contact-with-id-produces-json-response
+        (is (= 200 (:status (app (request :get (str "/contact/" id))))))
+        (is (= "{\"name\":{\"prefix\":\"\",\"family\":\"Gump\",\"suffix\":\"\",\"additional\":\"\",\"given\":\"Forrest\"},\"org\":\"Bubba Gump Shrimp Co.\",\"photo\":\"http:\\/\\/www.example.com\\/dir_photos\\/my_photo.gif\",\"title\":\"Shrimp Man\",\"email\":\"forrestgump@example.com\",\"rev\":\"20080424T195243Z\",\"address\":[{\"country\":\"United States of America\",\"locality\":\"Baytown\",\"code\":\"30314\",\"street\":\"42 Plantation St.\",\"type\":\"work\",\"region\":\"LA\",\"label\":\"42 Plantation St.\\nBaytown, LA 30314\\nUnited States of America\"}],\"formatted-name\":\"Forrest Gump\",\"tel\":[{\"type\":\"work,voice\",\"value\":\"+1-111-555-1212\"},{\"type\":\"home,voice\",\"value\":\"+1-404-555-1212\"}]}"
+               ;; remove the :_id as the response may have been created
+               ;; asas part of the test.
+               (-> (:body (app (request :get (str "/contact/" id))))
                    (json/read-json)
-                   (dissoc :id))))
-        (is (= 201 (:status response)))
-        (is (= "Dummy"
-               (:formatted-name (fetch-one :contacts
-                                           :where {:formatted-name "Dummy"}))))))))
+                   (dissoc :_id)
+                   (json/json-str)))))
+
+      (deftest contact-with-bad-id-produces-error-response
+        (is (= 404 (:status (app (request :get "/contact/meh")))))
+        (is (= "{\"error\":{\"message\":\"contact with id: 'meh' not found\"}}"
+               (:body (app (request :get "/contact/meh"))))))
+
+      (deftest contact-with-id-and-vcard-produces-vcard-response
+        (is (= 200 (:status (app (request :get (str "/contact/" id "/vcard"))))))
+        (is (= "BEGIN:VCARD\nVERSION:4.0\nN:Gump;Forrest;;;\nFN: Forrest Gump\nORG:Bubba Gump Shrimp Co.\nTITLE:Shrimp Man\nPHOTO:http://www.example.com/dir_photos/my_photo.gif\nTEL;TYPE=\"work,voice\";VALUE=uri:tel:+1-111-555-1212\nTEL;TYPE=\"home,voice\";VALUE=uri:tel:+1-404-555-1212\nADR;TYPE=work;LABEL=\"42 Plantation St.\nBaytown, LA 30314\nUnited States of America\"\n :;;42 Plantation St.;Baytown;LA;30314;United States of America\nEMAIL:forrestgump@example.com\nREV:20080424T195243Z\nEND:VCARD"
+               (:body (app (request :get (str "/contact/" id "/vcard"))))))))
+
+    (testing "POST /contact"
+      (deftest contact-is-created-on-post
+        (let [response (app (-> (request :post "/contact")
+                                (header "content-type" "application/x-www-form-urlencoded")
+                                (body (update-in test-record [:formatted-name] (fn [_] "Dummy")))))]
+          (is (= {"Content-Type" "application/json"} (:headers response)))
+          ;; Drop the :id key as this is likely to be dynamically generated
+          ;; for each test run.
+          (is (= {:message "contact created"}
+                 (-> (:body response)
+                     (json/read-json)
+                     (dissoc :id))))
+          (is (= 201 (:status response)))
+          (is (= "Dummy"
+                 (:formatted-name (fetch-one :contacts
+                                             :where {:formatted-name "Dummy"})))))))))
