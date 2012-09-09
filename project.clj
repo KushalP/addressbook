012d56e7e8a6bfa3f13329c8a3d0fbb4aca1cd3e
Kushal Pisavadia <kushi.p@gmail.com>
2012-09-09 11:52:30 +0100

    Bump monger to 1.2.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
                  [compojure "1.1.3"]
-                 [com.novemberain/monger "1.0.1"]
+                 [com.novemberain/monger "1.2.0"]
                  [com.novemberain/validateur "1.1.0"]
                  [de.ubercode.clostache/clostache "1.3.0"]
                  [ring/ring-jetty-adapter "1.1.0"]
