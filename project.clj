4a5bd55e3498a3098af45973c9650c6111239f07
Kushal Pisavadia <kushi.p@gmail.com>
2012-09-09 11:59:56 +0100

    Bump ring-json to 0.1.2

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [com.novemberain/validateur "1.2.0"]
                  [de.ubercode.clostache/clostache "1.3.0"]
                  [ring/ring-jetty-adapter "1.1.5"]
-                 [ring/ring-json "0.1.0"]]
+                 [ring/ring-json "0.1.2"]]
   :dev-dependencies [[lein-ring "0.7.1"]
                      [ring-mock "0.1.2"]]
   :main addressbook.core
