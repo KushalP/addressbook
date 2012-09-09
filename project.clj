b8c6e9c954abba16b5f69c5ff37440cb9516d990
Kushal Pisavadia <kushi.p@gmail.com>
2012-09-09 11:50:11 +0100

    Bump compojure to 1.1.3

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Restful web app which holds and retrieves contact information"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [compojure "1.1.0"]
+                 [compojure "1.1.3"]
                  [com.novemberain/monger "1.0.1"]
                  [com.novemberain/validateur "1.1.0"]
                  [de.ubercode.clostache/clostache "1.3.0"]
