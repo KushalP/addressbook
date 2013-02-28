7075394a055c406a56d05d6932d440e6059f9154
Kushal Pisavadia <kushi.p@gmail.com>
2013-02-28 17:51:19 +0000

    Update project config to use leiningen 2

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [de.ubercode.clostache/clostache "1.3.0"]
                  [ring/ring-jetty-adapter "1.1.5"]
                  [ring/ring-json "0.1.2"]]
-  :dev-dependencies [[lein-ring "0.7.5"]
-                     [ring-mock "0.1.3"]]
+  :profiles {:dev {:dependencies [[lein-ring "0.7.5"]
+                                  [ring-mock "0.1.3"]]}}
   :main addressbook.core
-  :ring {:handler addressbook.core/app})
+  :ring {:handler addressbook.core/app}
+  :test-selectors {:default (constantly true)})
