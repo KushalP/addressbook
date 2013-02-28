efecf8d293fa8c3fc7bf1207c9295c33ad2759c4
Kushal Pisavadia <kushi.p@gmail.com>
2013-02-28 18:14:02 +0000

    Upgrade compojure to 1.1.5

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A Restful web app which holds and retrieves contact information"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [compojure "1.1.3"]
+                 [compojure "1.1.5"]
                  [com.novemberain/monger "1.2.0"]
                  [com.novemberain/validateur "1.2.0"]
                  [de.ubercode.clostache/clostache "1.3.0"]
